3249f79778b949c4afdc96cb87864e66845c7f8e
jpSymfony <jpsymfony@free.fr>
2017-08-27 13:11:49 +0200

    added basic auth test with follow redirect

--- tests/AppBundle/Controller/BackendControllerTest.php
+++ tests/AppBundle/Controller/BackendControllerTest.php
@@ -7,7 +7,6 @@ use AppBundle\Test\WebTestCase;
 use AppBundle\Entity\Media;
 use Symfony\Component\BrowserKit\Cookie;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class BackendControllerTest extends WebTestCase
@@ -16,8 +15,6 @@ class BackendControllerTest extends WebTestCase
      * @var \Doctrine\ORM\EntityManager
      */
     private $em;
-
-    private $client = null;
 
     /**
      * {@inheritDoc}
@@ -29,17 +26,17 @@ class BackendControllerTest extends WebTestCase
         $this->em = static::$kernel->getContainer()
             ->get('doctrine')
             ->getManager();
-
-        $this->client = static::createClient();
     }
 
     public function testNewMediaForm()
     {
         $this->loadFixtures(self::$kernel);
+
         $client = static::createClient([], [
             'PHP_AUTH_USER' => 'admin',
             'PHP_AUTH_PW'   => 'password',
         ]);
+
         $client->followRedirects();
         $client->enableProfiler();
 
@@ -67,6 +64,82 @@ class BackendControllerTest extends WebTestCase
         );
 
         $this->assertEquals(1, $crawler->filter('html:contains("Votre media est enregistré")')->count());
+
+        $media = $this->em->getRepository(Media::class)->findOneBy(['url' => 'http://exh5266.cias.rit.edu/256/homework3/images/kitten.jpg']);
+        $this->assertNotNull($media);
+        $this->assertNull($media->getAverage());
+
+        // Check that the profiler is enabled
+        if ($profile = $client->getProfile()) {
+            // check the time spent in the framework
+            $this->assertLessThan(
+                600,
+                $profile->getCollector('time')->getDuration()
+            );
+        }
+
+        $client2->request('GET', '/show/11');
+
+        $this->assertEquals(
+            Response::HTTP_OK,
+            $client2->getResponse()->getStatusCode()
+        );
+    }
+
+    public function testNewMediaFormWithFollowRedirect()
+    {
+        $this->loadFixtures(self::$kernel);
+
+        $client = static::createClient();
+        $session = $client->getContainer()->get('session');
+
+        // the firewall context defaults to the firewall name
+        $firewallContext = 'backend';
+
+        $token = new UsernamePasswordToken('admin', 'password', $firewallContext, ['ROLE_ADMIN']);
+        $session->set('_security_'.$firewallContext, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $client->getCookieJar()->set($cookie);
+
+        $client->enableProfiler();
+
+        $client2 = static::createClient();
+        $client2->insulate();
+
+        $client->request('GET', '/admin');
+        $crawler = $client->followRedirect();
+
+        $this->assertEquals(
+            Response::HTTP_OK,
+            $client->getResponse()->getStatusCode()
+        );
+
+        $form = $crawler->selectButton('Valider')->form();
+        $form->setValues([
+            'media[title]' => 'trop trognon ce chaton',
+            'media[url]'    => 'http://exh5266.cias.rit.edu/256/homework3/images/kitten.jpg',
+        ]);
+
+        $client->submit($form);
+
+        if ($profile = $client->getProfile()) {
+            // check the number of requests
+            $this->assertEquals(
+                3,
+                $profile->getCollector('db')->getQueryCount()
+            );
+        }
+
+        $this->assertEquals($client->getContainer()->get('session')->getFlashBag()->get('success')[0], 'Votre media est enregistré');
+
+        $client->followRedirect();
+
+        $this->assertEquals(
+            Response::HTTP_OK,
+            $client->getResponse()->getStatusCode()
+        );
 
         $media = $this->em->getRepository(Media::class)->findOneBy(['url' => 'http://exh5266.cias.rit.edu/256/homework3/images/kitten.jpg']);
         $this->assertNotNull($media);
